bc0e4dcb40576877cf2b2354468463ee0afa580c
wlb-git <67083900+wlb-git@users.noreply.github.com>
2020-06-18 14:02:59 +0800

    这是一个药品管理项目

---
+++ DrugManagement_App/src/main/java/com/bdqn/DrugManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.bdqn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrugManagementAppApplication.class, args);
+	}
+
+}
